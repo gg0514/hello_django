9b85ecb4afd4177accfb1caca6152d83c1af2c6b
gg0514 <gg0514g@gmail.com>
2023-07-11 10:54:16 +0900

    1st commit

---
+++ app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+# path() 함수 : URL string, View Function, URL Pattern Name
+urlpatterns= [    
+    path('', views.base, name='base'),                       # 127.0.0.1:8000/app/
+    path('hello/', views.greeting, name='hello'),            # 127.0.0.1:8000/app/hello/
+]
