c36484f5becf7673404a14213f2ba366af3b342f
gg0514 <gg0514g@gmail.com>
2023-07-11 16:41:36 +0900

    modify db field

--- myapp/admin.py
+++ myapp/admin.py
@@ -2,4 +2,8 @@
 from django.contrib import admin
 from .models import Member
 
-admin.site.register(Member)
+
+class MemberAdmin(admin.ModelAdmin):
+  list_display = ("firstname", "lastname", "joined_date",)
+  
+admin.site.register(Member, MemberAdmin)
