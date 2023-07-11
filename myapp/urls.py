3d4059f8d2f12a8d5dce16e6403a052b50208f4f
gg0514 <gg0514g@gmail.com>
2023-07-11 15:36:48 +0900

    add main.html

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 # path() 함수 : URL string, View Function, URL Pattern Name
 urlpatterns= [    
-    path('', views.base, name='base'),                       # 127.0.0.1:8000/app/
-    path('hello/', views.greeting, name='hello'),            # 127.0.0.1:8000/app/hello/
-    path('members/', views.members, name='members'),           # 127.0.0.1:8000/app/member/
-    path('members/details/<int:id>', views.details, name='details'),    
+    path('', views.main, name='main'),                                      # 127.0.0.1:8000/
+    path('hello/', views.greeting, name='hello'),                           # 127.0.0.1:8000/hello/
+    path('members/', views.members, name='members'),                        # 127.0.0.1:8000/members/
+    path('members/details/<int:id>', views.details, name='details'),        # 127.0.0.1:8000/details/5
 ]
